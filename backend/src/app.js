7245bf2acbc314015d9ad1a0921ed060b3688b92
Kareem Abayomi <kareemherbaryourme@gmail.com>
2026-09-23 16:34:33 +0100

    chore: initialize PhoenixTASKFLOW project

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "PhoenixTASKFLOW API is running",
+    data: null,
+  });
+});
+
+module.exports = app;
